02c0f3fdc025e339087706c388a1ec6dab061e85
ErfanParasti <108697797+ErfanParasti@users.noreply.github.com>
2022-12-04 16:30:45 +0330

    Add files via upload

---
+++ script.js
@@ -0,0 +1,224 @@
+
+
+
+// function anymtion_test_fun(){
+//     document.getElementById("mydiv").style.marginTop="400px"
+//     }
+    
+
+
+
+//     function anymtion_test2_fun(){
+//         document.getElementById("mydiv").style.marginTop="0px"
+//     }
+const $ = document
+const bar = document.querySelector(".bar");
+const bar1 = document.querySelector(".bar1");
+const bar2 = document.querySelector(".bar2");
+const bar3 = document.querySelector(".bar3");
+const my_photos = document.getElementById("my_photos");
+const names =  $.getElementById("name")
+const year_of_birth =$.getElementById("year_of_birth")
+const age = $.getElementById("age")
+const about_me = $.getElementById("about_me")
+const hr_1 = $.getElementById("hr_1")
+const hr_2 = $.getElementById("hr_2")
+const hr_3 = $.getElementById("hr_3")
+const hr_4 = $.getElementById("hr_4")
+const summary = $.getElementById("summary")
+const summary_p = $.getElementById("summary_p")
+const Ways_of_communication = $.getElementById("Ways_of_communication")
+const Skill = $.getElementById("Skill")
+const work_experience = $.getElementById("work_experience")
+const work_experience_p = $.getElementById("work_experience_p")
+const icone = $.getElementById("icone")
+const Layer_1 = $.getElementById("Layer_1")
+const Layer_2 = $.getElementById("Layer_2")
+
+setTimeout(() => {
+  my_photos.style.width="150px"
+  my_photos.style.height="150px"
+  
+
+}, 500);
+
+setTimeout(() => {
+  about_me.style.opacity="100%"
+
+  about_me.style.animationName="about_me"
+
+  
+
+}, 1000);
+
+setTimeout(() => {
+  names.style.opacity="100%"
+
+  names.style.animationName="name"
+
+  
+
+}, 1500);
+
+setTimeout(() => {
+  year_of_birth.style.opacity="100%"
+
+  year_of_birth.style.animationName="year_of_birth"
+
+  
+
+}, 1700);
+setTimeout(() => {
+  Layer_1.style.opacity="100%"
+
+  Layer_1.style.animationName="Layer_1"
+
+  
+
+}, 7500);
+setTimeout(() => {
+  age.style.opacity="100%"
+  
+  age.style.animationName="age"
+
+  
+
+}, 2200);
+
+setTimeout(() => {
+  hr_1.style.opacity="100%"
+  
+  hr_1.style.animationName="hr_1"
+
+  
+
+}, 3000);
+
+setTimeout(() => {
+  summary.style.opacity="100%"
+  
+  summary.style.animationName="summary"
+
+  
+
+}, 2900);
+
+setTimeout(() => {
+  summary_p.style.opacity="100%"
+  
+  summary_p.style.animationName="summary_p"
+
+  
+
+}, 3200);
+
+setTimeout(() => {
+  hr_2.style.opacity="100%"
+  
+  hr_2.style.animationName="hr_2"
+
+  
+
+}, 3500);
+
+setTimeout(() => {
+  Ways_of_communication.style.opacity="100%"
+  
+  Ways_of_communication.style.animationName="Ways_of_communication"
+
+  
+
+}, 8000);
+
+
+setTimeout(() => {
+  Skill.style.opacity="100%"
+  
+  Skill.style.animationName="Skill"
+
+  
+
+}, 4500);
+
+setTimeout(() => {
+  my_photos.style.width="150px"
+  my_photos.style.height="150px"
+  
+
+}, 5000);
+
+setTimeout(() => {
+  my_photos.style.width="150px"
+}, 5200);
+setTimeout(() => {
+  bar.style.setProperty("--progress", "95%");
+}, 5300);
+setTimeout(() => {
+    bar1.style.setProperty("--progress", "75%");
+  }, 5400);
+setTimeout(() => {
+    bar2.style.setProperty("--progress", "75%");
+  }, 5500);
+setTimeout(() => {
+      bar3.style.setProperty("--progress", "50%");
+    }, 5600);
+setTimeout(() => {
+  work_experience.style.opacity="100%"
+      
+  work_experience.style.animationName="work_experience"
+  
+      
+    
+  }, 7000);
+  setTimeout(() => {
+    work_experience_p.style.opacity="100%"
+        
+    work_experience_p.style.animationName="work_experience_p"
+    
+        
+      
+    }, 7200);
+setTimeout(() => {
+  hr_3.style.opacity="100%"
+        
+  hr_3.style.animationName="hr_3"
+    
+        
+      
+}, 6000);
+      
+setTimeout(() => {
+  hr_4.style.opacity="100%"
+        
+  hr_4.style.animationName="hr_4"
+    
+        
+      
+}, 6500);
+      
+setTimeout(() => {
+  icone.style.opacity="100%"
+  icone.style.marginLeft="125px"
+  icone.style.animationName="icone"
+        
+      
+}, 6500);
+      
+
+
+
+function like_on(){
+
+    Layer_2.style.display="block"
+    Layer_1.style.display="none"
+        
+  
+        
+
+
+}
+function like_off(){
+  Layer_1.style.display="block"
+  Layer_2.style.display="none"
+
+}
